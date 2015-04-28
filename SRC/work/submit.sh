e2d66246f489d93f63acb0bfb92470a554544f5a
nick <ncollier@anl.gov>
2015-04-28 07:04:22 -0400

    latest

--- SRC/work/submit.sh
+++ SRC/work/submit.sh
@@ -40,5 +40,5 @@ module load repast-hpc/2.2-dev
 
 # 8. Run our MPI job.  GERun is a wrapper that launches MPI jobs on Legion  
 # This assumes the .props files have been copied to your working directory.
-echo $1 $2 $3 $4
-gerun $3 $4 $5
+#echo $1 $2 $3 $4
+gerun $1 $2 $3
